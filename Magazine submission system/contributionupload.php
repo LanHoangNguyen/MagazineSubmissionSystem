5de2a2fed3a07744ff6c8a2d4f36ea8d4132f202
LanHoangNguyen <55873544+LanHoangNguyen@users.noreply.github.com>
2021-03-29 19:16:11 +0700

    Update contributionupload.php

--- Magazine submission system/contributionupload.php
+++ Magazine submission system/contributionupload.php
@@ -1,30 +1,31 @@
 <?php
 include("config.php");
 include("session.php");
-if (isset($_POST['upload'])){
-	$uploaddir = '/home/postgres/';
-    $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-    $filename = $_POST['filename'];
-    if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile))
-    {
-	    echo "File is valid, and was successfully uploaded.\n";
-    }
-    else   
-	{
-		echo "Invalid file, please choose another one\n\n";
-	}
-	$loggeduserid=$user_check['userid'];
-	$query="insert into contributions(contributionname, contributionfile, userid) values ('$filename',pg_lo_import('$uploadfile'),'$loggeduserid')";
-	$result=pg_query($dbconn,$query);
-	if($result){
-		echo "File is successfully upload";
-	}
-	else
-	{
-		echo "Upload failed, try again";
-	}
+if (isset($_POST['save'])){
+	 $loggeduserid=$user_check['userid'];
+	 $filename = $_FILES['myfile']['name'];
+	 $destination = 'uploads/' . $filename;
+	 $extension = pathinfo($filename, PATHINFO_EXTENSION);
+	 $file = $_FILES['myfile']['tmp_name'];
+	 $size = $_FILES['myfile']['size'];
+
+	 if (!in_array($extension, ['zip', 'pdf', 'docx','png','jpeg'])){
+	 	echo "You file extension must be .zip, .pdf, .docx, .png or jpeg";
+	 } elseif ($_FILES['myfile']['size'] > 1000000){
+	 	echo "File too large!";
+	 } else{
+	 	if (move_uploaded_file($file, $destination)){
+	 		$sql="insert into contributions(contributionname,userid) values ('$filename',$loggeduserid)";
+	 		if(pg_query($dbconn,$sql)){
+	 			echo "File upload successfully";
+	 		} else{
+	 			echo "Failed to upload file";
+	 		}
+	 	}
+
+	 }
 }
- ?>
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -33,18 +34,39 @@ if (isset($_POST['upload'])){
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="./bootstrap-5.0.0-beta2-dist/css/bootstrap.css">
     <script src="./bootstrap-5.0.0-beta2-dist/js/bootstrap.js"></script>
+	<style type="text/css">
+    	form {
+            width: 30%;
+            margin: 100px auto;
+            padding: 30px;
+            border: 1px solid #555;
+        }
+        input {
+            width: 100%;
+            border: 1px solid #f1e1e1;
+            display: block;
+            padding: 5px 10px;
+        }
+        button {
+            border: none;
+            padding: 10px;
+            border-radius: 5px;
+        }
+        table {
+            width: 60%;
+            border-collapse: collapse;
+            margin: 100px auto;
+        }
+    </style>
 </head>
 <body>
 	<div class=container>
 		<h2>File and image upload</h2>
 		<form method="POST" enctype="multipart/form-data">
-			<div class="form-group">
-				<label>Name</label>
-				<input type="text" name="filename" id="filename">
-			</div>
+			<h3>Upload File</h3>
 			<div class="form-group">
 				<label>File</label>
-				<input type="file" name="userfile" id="userfile">
+				<input type="file" name="myfile">
 			</div>
 			<input type="submit" name="upload" value="upload" class="btn btn-primary">
 		</form>
