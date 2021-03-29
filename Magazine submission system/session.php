e8b3c36ae4db33833c04a51b81ceee4cd23a8272
LanHoangNguyen <55873544+LanHoangNguyen@users.noreply.github.com>
2021-03-29 20:53:26 +0700

    Update session.php

--- Magazine submission system/session.php
+++ Magazine submission system/session.php
@@ -6,6 +6,7 @@ $sql="select username from public.users where username='$user_check' ";
 $ses_sql=pg_query($dbconn,$sql);
 $row=pg_fetch_array($ses_sql,PGSQL_ASSOC);
 $login_session=$row['username'];
+$loggeduserid=$row['userid'];
 if(!isset($_SESSION['login_user'])){
 	header("Location:login.php");
 	die();
