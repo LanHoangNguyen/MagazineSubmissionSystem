86b23629c4ea3c98a61b20cc9d830b2be30b852a
LanHoangNguyen <55873544+LanHoangNguyen@users.noreply.github.com>
2021-03-31 23:00:14 +0700

    Delete Magazine submission system directory

--- Magazine submission system/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$port = "5432";
-$dbname = "postgres";
-$user = "postgres";
-$password = "postgres"; 
-$connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
-$dbconn = pg_connect($connection_string);
-?>
