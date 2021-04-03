466b239b09b12118e770ca5993e29eb4a3e4660d
LanHoangNguyen <55873544+LanHoangNguyen@users.noreply.github.com>
2021-04-03 16:18:28 +0700

    Delete Magazine submission system directory

--- Magazine submission system/contributiondelete.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('config.php');
-$contributionid=$_REQUEST['contributionid'];
-$query="delete from contributions where contributionid=$contributionid ";
-$result=pg_query($dbconn,$query);
-?>
--- Magazine submission system/deleteacount.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('config.php');
-$userid=$_REQUEST['userid'];
-$query="delete form users where userid=$userid";
-$result=pg_query($dbconn,$query);
-header("Location: accountlisting.php")
-?>
